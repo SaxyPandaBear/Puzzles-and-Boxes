18daf900ccf96f51116cbe3901298ac3933fb0a2
SaxyPandaBear <ahuynh11@masonlive.gmu.edu>
2016-02-24 20:27:09 -0500

    Initial Commit
    Eh will leave this on the back burner for now

--- js/main.js
+++ js/main.js
@@ -15,38 +15,32 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
+    //vars used throughout the game
+    var blocks; //overarching block group
+    var goalBlock; //this is the block that we need to get through the "exit"
+    var bounds; //add bounds to the outside of the map, but then make sure there is a gap for the goal block to pass through
+    
+    var movesLeft = 25; //determine a set number of moves that the player has before the game terminates
+    
+    var fader; //make a way to darken the screen as time goes on - using an opacity based tween
+    
+    var movesText; //text that displays how many moves the player has left before the game ends
+    var timeText; //text that displays how much time has elapsed in the game (or possibly how much time is left)
+    
+    var goodSound, badSound; //good = completed the puzzle in the time given, bad = time ran out
+    var music; //background music that plays as the game progresses.
+    
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image("assets/images/block1.png"); //horizontal
+        game.load.image("assets/images/block2.png"); //vertical
+        game.load.image("assets/images/background.png"); //background image 
     }
-    
-    var bouncy;
-    
+        
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+       
     }
 };
